73a5f2199200814668aa941bb7bb8f4a21c4e3d9
Stéphane LEGOUFFE <slegouffe@gmail.com>
2019-03-20 12:01:34 +0100

    Missing file

--- src/store/dataset/getters.js
+++ src/store/dataset/getters.js
@@ -7,13 +7,16 @@ export const getDataset = (state) => {
   if (state.data) {
     _.each(state.dataset, (data) => {
       data.isActive = (data._id === state.data._id);
+      if (data.usersAnswers && data.usersAnswers.length > 0 && !data.usersAnswers[0].label) {
+        data.usersAnswers = _.map(data.usersAnswers, answer => JSON.parse(answer.answers));
+      }
     });
   }
   return state.dataset;
 };
 
 export const getData = (state) => {
-  if (state.data) {
+  if (state.data && state.data.metadata) {
     const rawMetadata = state.data.metadata.raw;
     if (rawMetadata && typeof rawMetadata === 'string') {
       state.data.metadata.raw = JSON.parse(rawMetadata);
