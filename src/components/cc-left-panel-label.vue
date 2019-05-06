bd32f29653301ef69e72fb60fd4ab210770733d8
Stéphane LEGOUFFE <slegouffe@gmail.com>
2019-05-06 10:23:06 +0200

    Enlever les animations slide in / slide out

--- src/components/cc-left-panel-label.vue
+++ src/components/cc-left-panel-label.vue
@@ -4,91 +4,70 @@
       Si la photo ci-contre contient l'un des éléments suivants,
       sélectionnez l'objet que vous allez identifier sur l'image
     </q-list-header>
-    <transition-group enter-active-class="animated slideInLeft"
-                      leave-active-class="animated slideOutLeft">
-      <q-item key="pickup" v-if="pickUpLabel" style="text-align: center;">
-        <q-item-main>
-          <q-item-tile v-for="(label, index) in labels"
-                       v-if="label.id !== 'none'"
-                       :key="index"
-                       label>
-            <q-btn :label="label.label"
-                  @click="onSelect(label)"
+    <q-item key="pickup" v-if="pickUpLabel" style="text-align: center;">
+      <q-item-main>
+        <q-item-tile v-for="(label, index) in labels"
+                      v-if="label.id !== 'none'"
+                      :key="index"
+                      label>
+          <q-btn :label="label.label"
+                @click="onSelect(label)"
+                class="full-width"
+                style="margin-top: 10px;"
+                color="pink">
+                <q-tooltip>
+                  <img :src="label.img" style="width: 200px;" />
+                </q-tooltip>
+          </q-btn>
+        </q-item-tile>
+        <q-item-tile>
+          <q-btn label="passer à la photo suivante"
+                @click="onNext()"
+                class="full-width"
+                style="margin-top: 10px;"
+                color="dark" />
+        </q-item-tile>
+      </q-item-main>
+    </q-item>
+    <q-item v-if="pickUpLabelConfirm"
+            key="confirmBlock"
+            style="text-align: center;">
+      <q-item-main>
+        <q-item-tile label>
+          vous avez sélectionné
+        </q-item-tile>
+        <q-item-tile style="margin-top: 10px;">
+          <q-chip @hide="onReset()" closable color="dark">
+            {{ label.label }}
+          </q-chip>
+        </q-item-tile>
+        <q-item-tile style="padding-top: 10px;">
+          Vous pouvez commencer à identifier l'objet dans l'image.
+        </q-item-tile>
+        <q-item-tile style="padding-top: 10px;">
+          <a @click="onNext()" class="next">passer à l'image suivante</a>
+        </q-item-tile>
+      </q-item-main>
+    </q-item>
+    <q-item key="nextLabel" v-if="nextLabel" style="text-align: center;">
+      <q-item-main>
+        <q-item-tile label>
+          <strong>Merci pour votre contribution !</strong>
+        </q-item-tile>
+        <q-item-tile style="margin-top: 10px;">
+          <q-btn @click="onSaveAndContinue()"
+                  label="continuer avec cette image"
                   class="full-width"
                   style="margin-top: 10px;"
-                  color="pink">
-                  <q-tooltip>
-                    <img :src="label.img" style="width: 200px;" />
-                  </q-tooltip>
-            </q-btn>
-          </q-item-tile>
-          <q-item-tile>
-            <q-btn label="passer à la photo suivante"
-                  @click="onNext()"
-                  class="full-width"
-                  style="margin-top: 10px;"
-                  color="dark" />
-          </q-item-tile>
-        </q-item-main>
-      </q-item>
-    </transition-group>
-    <transition-group
-        appear
-        enter-active-class="animated slideInLeft"
-        leave-active-class="animated slideOutLeft" >
-      <q-item v-if="pickUpLabelConfirm"
-              key="confirmBlock"
-              style="text-align: center;">
-        <q-item-main>
-          <q-item-tile label>
-            vous avez sélectionné
-          </q-item-tile>
-          <q-item-tile style="margin-top: 10px;">
-            <q-chip @hide="onReset()" closable color="dark">
-              {{ label.label }}
-            </q-chip>
-          </q-item-tile>
-          <q-item-tile style="padding-top: 10px;">
-            Vous pouvez commencer à identifier l'objet dans l'image.
-          </q-item-tile>
-          <q-item-tile style="padding-top: 10px;">
-            <a @click="onNext()" class="next">passer à l'image suivante</a>
-          </q-item-tile>
-          <!-- <q-item-tile style="margin-top: 10px;">
-            <q-btn @click="onSave()"
-                    v-if="canContribute"
-                    label="contribuer !"
-                    class="full-width"
-                    style="margin-top: 10px;"
-                    color="positive" />
-          </q-item-tile> -->
-        </q-item-main>
-      </q-item>
-    </transition-group>
-    <transition-group
-        appear
-        enter-active-class="animated slideInLeft"
-        leave-active-class="animated slideOutLeft">
-        <q-item key="nextLabel" v-if="nextLabel" style="text-align: center;">
-          <q-item-main>
-            <q-item-tile label>
-              <strong>Merci pour votre contribution !</strong>
-            </q-item-tile>
-            <q-item-tile style="margin-top: 10px;">
-              <q-btn @click="onSaveAndContinue()"
-                     label="continuer avec cette image"
-                     class="full-width"
-                     style="margin-top: 10px;"
-                     color="pink" />
-              <q-btn @click="onSave()"
-                    label="passer à l'image suivante"
-                    class="full-width"
-                    style="margin-top: 10px;"
-                    color="dark" />
-            </q-item-tile>
-          </q-item-main>
-        </q-item>
-    </transition-group>
+                  color="pink" />
+          <q-btn @click="onSave()"
+                label="passer à l'image suivante"
+                class="full-width"
+                style="margin-top: 10px;"
+                color="dark" />
+        </q-item-tile>
+      </q-item-main>
+    </q-item>
   </q-list>
 </template>
 
@@ -119,14 +98,10 @@ export default {
     canContribute(newVal) {
       if (newVal) {
         this.pickUpLabelConfirm = false;
-        setTimeout(() => {
-          this.nextLabel = true;
-        }, 1000);
+        this.nextLabel = true;
       } else {
         this.nextLabel = false;
-        setTimeout(() => {
-          this.pickUpLabel = true;
-        }, 1000);
+        this.pickUpLabel = true;
       }
     },
   },
@@ -142,17 +117,13 @@ export default {
       this.label = selectedLabel;
       this.$store.commit('label/SET_LABEL', this.label);
       this.pickUpLabel = false;
-      setTimeout(() => {
-        this.pickUpLabelConfirm = true;
-      }, 1000);
+      this.pickUpLabelConfirm = true;
     },
     onReset() {
       this.label = null;
       this.$store.commit('label/SET_LABEL', null);
       this.pickUpLabelConfirm = false;
-      setTimeout(() => {
-        this.pickUpLabel = true;
-      }, 1000);
+      this.pickUpLabel = true;
     },
     setImg(img) {
       return `${process.env.API_URL}/img/${this.projectId}/${img}`;
